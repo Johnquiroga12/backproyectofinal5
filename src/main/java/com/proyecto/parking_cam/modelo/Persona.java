d6b9e16eef50e6c7a5f77b7cf8778c31ef460141
Ariel Aguirre <ariel.aguirre.est@tecazuay.edu.ec>
2023-02-24 00:33:46 -0500

    actualización relaciones

--- src/main/java/com/proyecto/parking_cam/modelo/Persona.java
+++ src/main/java/com/proyecto/parking_cam/modelo/Persona.java
@@ -38,6 +38,7 @@ public class Persona implements Serializable {
     @JsonIgnore
     @OneToOne(mappedBy = "persona")
     private Usuario usuario;
+    @JsonIgnore
     @OneToMany(mappedBy = "persona")
      private List<Vehiculo> vehiculo;
 

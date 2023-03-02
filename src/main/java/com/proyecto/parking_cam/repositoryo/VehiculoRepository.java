94b7302e86e333a0454b7817957d770826d3fee4
Chimbo-K18 <kevin.chimbo.est@tecazuay.edu.ec>
2023-03-02 03:35:52 -0500

    Revert "bbcito"
    This reverts commit aad72d87449a09fb7c74aaa259106279b6c0ef5f.

--- src/main/java/com/proyecto/parking_cam/repositoryo/VehiculoRepository.java
+++ src/main/java/com/proyecto/parking_cam/repositoryo/VehiculoRepository.java
@@ -1,20 +1,12 @@
 package com.proyecto.parking_cam.repositoryo;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.proyecto.parking_cam.modelo.Bloque;
 import com.proyecto.parking_cam.modelo.Vehiculo;
 
 @Repository
 public interface VehiculoRepository  extends JpaRepository<Vehiculo, Integer>{
-	
-	
-	public Vehiculo findByPlaca(String placa);
-	
+
 }

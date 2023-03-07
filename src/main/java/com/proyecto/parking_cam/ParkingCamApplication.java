a1026e6eb4a547d1dfaf7f14e0076bc6b7d4c300
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-03-07 00:57:02 -0500

    new changes
    new changes

--- src/main/java/com/proyecto/parking_cam/ParkingCamApplication.java
+++ src/main/java/com/proyecto/parking_cam/ParkingCamApplication.java
@@ -1,5 +1,6 @@
 package com.proyecto.parking_cam;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ParkingCamApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/proyecto/parking_cam/modelo/Persona.java
+++ src/main/java/com/proyecto/parking_cam/modelo/Persona.java
@@ -3,7 +3,7 @@ package com.proyecto.parking_cam.modelo;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 
 @Getter
